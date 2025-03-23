bc90d9f7fb3f58915b79bd6fd7c58a2d4047954d
unrelentin <augmentedvisionjcd@gmail.com>
2025-03-23 07:15:22 +0700

    added modal

--- script.js
+++ script.js
@@ -1,107 +1,178 @@
-const myLibrary = [];
+const gameLibrary = [];
 
-function Book (title, author, page, readStatus) {
-    this.title = title;
-    this.author = author;
-    this.page = page;
-    this.readStatus = readStatus;
-    
-    this.announceBook = function () {
-        console.log(`${this.name}` + ", " + `${this.author}` + ", " + `${this.page}` + ", " + `${this.readStatus}`)
-    }
+class Game {
+  constructor(name, developer, playTime, clearStatus, image) {
+    this.name = name;
+    this.developer = developer;
+    this.playTime = playTime;
+    this.clearStatus = clearStatus;
+    this.image = image;
+  }
+
+  toggleClearStatus() {
+    this.clearStatus = this.clearStatus === "Played" ? "Not Played" : "Played";
+  }
 }
 
-function addBookToLibrary () {
-    /*take some arguments, create a book from those arguments, and store the new book object into an array */
-    const title = document.querySelector("#book_title").value;
-    const author = document.querySelector("#book_author").value;
-    const page = document.querySelector("#book_page").value;
-    const readStatus = document.querySelector(`input[name="book_status"]:checked`).value;           //here's how you get the radio button
+function addGameToLibrary() {
+  const name = document.querySelector("#game_name").value;
+  const developer = document.querySelector("#game_dev").value;
+  const playTime = document.querySelector("#game_playtime").value;
+  const clearStatus = document.querySelector(
+    `input[name="clear_status"]:checked`,
+  ).value;
+  const image = document.querySelector(`#game_image`).files[0];
 
-    const newBook = new Book (title, author, page, readStatus);
-
-    myLibrary.push(newBook);
-
+  if (image) {
+    const imageReader = new FileReader();
+    imageReader.readAsDataURL(image);
+    imageReader.onload = function (event) {
+      const imageSrc = event.target.result;
+      const newGame = new Game(
+        name,
+        developer,
+        playTime,
+        clearStatus,
+        imageSrc,
+      );
+      gameLibrary.push(newGame);
+      gameDisplay(); //Need to call this because FileReader is async
+    };
+  } else {
+    const newGame = new Game(
+      name,
+      developer,
+      playTime,
+      clearStatus,
+      "pics/Vergil.jpg",
+    );
+    gameLibrary.push(newGame);
+    gameDisplay();
+  }
 }
 
-//DISPLAYING THE BOOK
-const theBook = document.querySelector(".main_content");
+const displayOfGame = document.querySelector(".game_pool");
 
-function displayBook () {
-    
-    theBook.innerHTML = "";
+function gameDisplay() {
+  displayOfGame.innerHTML = "";
 
-    myLibrary.forEach((book, index) => {                                          //use forEach to iterate through every instance
-        const bookCard = document.createElement("div");
-        bookCard.classList.add("book_card");
-        bookCard.setAttribute("data_index", index);                         //gotta set before you get
+  gameLibrary.forEach((game, index) => {
+    //iterate through each entry while keeping track of the index
+    const gameCard = document.createElement("div");
+    gameCard.classList.add("gameCard");
+    gameCard.setAttribute("data_index", index); //give each entry an index
 
-        const bookTitle = document.createElement("div");
-        bookTitle.classList.add("book_title");
-        bookTitle.textContent = `Title: ${book.title}`;
-        bookCard.appendChild(bookTitle);
+    const buttonContainer = document.createElement("button");
+    buttonContainer.classList.add("buttonContainer");
+    gameCard.appendChild(buttonContainer);
 
-        const bookAuthor = document.createElement("div");
-        bookAuthor.classList.add("book_author");
-        bookAuthor.textContent = `Author: ${book.author}`;
-        bookCard.appendChild(bookAuthor);
+    const detailsContainer = document.createElement("div");
+    detailsContainer.classList.add("detailsContainer");
+    gameCard.appendChild(detailsContainer);
 
-        const bookPage = document.createElement("div");
-        bookPage.classList.add("book_page");
-        bookPage.textContent = `Book's pages: ${book.page}`;
-        bookCard.appendChild(bookPage);
+    const gameName = document.createElement("div");
+    gameName.classList.add("gameName");
+    gameName.textContent = `Game Title: ${game.name}`;
+    detailsContainer.appendChild(gameName);
 
-        const bookStatus = document.createElement("button");
-        bookStatus.classList.add("book_status");
-        bookStatus.textContent = `${book.readStatus}`;
-        bookCard.appendChild(bookStatus);
+    const gameDev = document.createElement("div");
+    gameDev.classList.add("gameDev");
+    gameDev.textContent = `Game Developer: ${game.developer}`;
+    detailsContainer.appendChild(gameDev);
 
-        const removeButton = document.createElement("button");
-        removeButton.classList.add("remove_button");
-        removeButton.textContent = `Remove`;
-        bookCard.appendChild(removeButton);
+    const gameTime = document.createElement("div");
+    gameTime.classList.add("gameTime");
+    gameTime.textContent = `Completion Time:` + game.playTime + ` hours`;
+    detailsContainer.appendChild(gameTime);
 
+    const gameImage = document.createElement("img");
+    gameImage.classList.add("gameImage");
+    gameImage.src = game.image;
+    gameCard.appendChild(gameImage);
 
-        theBook.appendChild(bookCard);
-    });
+    const gameStatus = document.createElement("button");
+    gameStatus.classList.add("gameStatus");
+    gameStatus.textContent = `${game.clearStatus}`;
+    buttonContainer.appendChild(gameStatus);
+    game;
+
+    const remove_button = document.createElement("button");
+    remove_button.classList.add("remove_button");
+    remove_button.textContent = "X";
+    buttonContainer.appendChild(remove_button);
+
+    displayOfGame.appendChild(gameCard);
+  });
 }
 
-// ACCEPT THE FORM'S INPUT
-document.querySelector(".book_add_form").addEventListener("submit", (event) => {
-    event.preventDefault();
-    addBookToLibrary();
-    displayBook();
-    document.querySelector(".book_add_form").reset();
-    modal.close(); //added another modal closing function to the submit button
+document.querySelector(".add_game_form").addEventListener("submit", (event) => {
+  event.preventDefault(); //prevent default because it will send data to a server by default, there's no server here
+  addGameToLibrary();
+  gameDisplay();
+  document.querySelector(".add_game_form").reset();
+  modal.close();
 });
 
-//MAKE THE FORM A MODAL
 const modal = document.querySelector("#modal");
-const openModal = document.querySelector(".open-button");
+const openModal = document.querySelector(".new_game");
 
 openModal.addEventListener("click", () => {
-    modal.showModal();
+  modal.showModal();
 });
 
-//REMOVE BUTTON
-theBook.addEventListener("click", (event)=> {
-    if (event.target.classList.contains("remove_button")) {
-        const bookIndex = event.target.parentElement.getAttribute("data_index");
-        myLibrary.splice(bookIndex, 1);
-        displayBook();
-    }
+window.onclick = function (event) {
+  if (event.target === modal){
+    modal.close()
+  };
+}
+
+displayOfGame.addEventListener("click", (event) => {
+  //apply to displayOfGame and not remove button directly because of event delegation, more efficient
+  if (event.target.classList.contains("remove_button")) {
+    const gameIndex = event.target.parentElement.getAttribute("data_index");
+    gameLibrary.splice(gameIndex, 1);
+    gameDisplay();
+  }
 });
 
-//READ STATUS BUTTON
-
-Book.prototype.toggleReadStatus = function() {
-    this.readStatus = this.readStatus === "Read" ? "Not Read" : "Read";
-};
-
-theBook.addEventListener("click", (event) => {
-    if (event.target.classList.contains("book_status")) {
-        const bookIndex = event.target.parentElement.getAttribute("data_index");
-        myLibrary[bookIndex].toggleReadStatus();                           // call it on the card which is found by the index
-        displayBook();
-    }
+displayOfGame.addEventListener("click", (event) => {
+  if (event.target.classList.contains("gameStatus")) {
+    const gameIndex =
+      event.target.parentElement.parentElement.getAttribute("data_index");
+    gameLibrary[gameIndex].toggleClearStatus(); //call the method on the currently selected index
+    gameDisplay();
+  }
 });
+
+gameLibrary.push(
+  new Game("Half Life", "Valve", 11.5, "Played", "pics/Half-Life.jpg"),
+);
+gameLibrary.push(
+  new Game(
+    "Resident Evil",
+    "Capcom",
+    10,
+    "Not Played",
+    "pics/Resident_Evil_1.png",
+  ),
+);
+gameLibrary.push(
+  new Game(
+    "Fallout New Vegas",
+    "Obsidian",
+    35,
+    "Played",
+    "pics/Fallout_New_Vegas.jpg",
+  ),
+);
+gameLibrary.push(
+  new Game(
+    "Sekiro: Shadow die Twice",
+    "From Sofware",
+    20,
+    "Played",
+    "pics/Sekiro.jpg",
+  ),
+);
+
+gameDisplay();
